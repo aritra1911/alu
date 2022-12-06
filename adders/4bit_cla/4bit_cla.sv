75d4e8f73de550d3ad20790d91cbe8528d460d29
Aritra Sarkar <aritra1911@yahoo.com>
2022-12-06 15:07:01 +0530

    Implement a 4-bit Carry-Lookahead adder

---
+++ adders/4bit_cla/4bit_cla.sv
@@ -0,0 +1,39 @@
+`ifndef __4BIT_CLA_SVH_
+`define __4BIT_CLA_SVH_
+
+`default_nettype none
+`timescale 1us/100 ns
+
+module _4bit_cla(a, b, c_in, s, c_out);
+
+    input wire [3:0] a, b;
+    input wire c_in;
+    output wire [3:0] s;
+    output wire c_out;
+
+    wire [3:0] p, g, c;
+
+    assign p = a ^ b;
+    assign g = a & b;
+
+    assign c[0]  = c_in;
+
+    assign c[1]  = g[0] | (p[0] & c[0]);
+
+    assign c[2]  = g[1] | (p[1] & g[0])
+                        | (p[1] & p[0] & c[0]);
+
+    assign c[3]  = g[2] | (p[2] & g[1])
+                        | (p[2] & p[1] & g[0])
+                        | (p[2] & p[1] & p[0] & c[0]);
+
+    assign c_out = g[3] | (p[3] & g[2])
+                        | (p[3] & p[2] & g[1])
+                        | (p[3] & p[2] & p[1] & g[0])
+                        | (p[3] & p[2] & p[1] & p[0] & c[0]);
+
+    assign s = c ^ p;
+
+endmodule
+
+`endif  /* __4BIT_CLA_SVH_ */
---
+++ adders/4bit_cla/4bit_cla_formal.sv
@@ -0,0 +1,23 @@
+`default_nettype none
+`timescale 1us/100 ns
+
+`include "4bit_cla.sv"
+
+module _4bit_cla_formal(a, b, c_in, s, c_out);
+
+    input wire [3:0] a, b;
+    input wire c_in;
+    output wire [3:0] s;
+    output wire c_out;
+
+    _4bit_cla cla(.a(a), .b(b), .c_in(c_in), .s(s), .c_out(c_out));
+
+    wire [4:0] y;
+    assign y = a + b + c_in;
+
+    always @(a, b, c_in) begin
+        assert(s == y[3:0] && c_out == y[4]);
+        cover(s == 4'hf && c_out);
+    end
+
+endmodule
